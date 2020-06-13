8333a29a393011a065d0345e2ff6715c8e29d040
Jeunn Hao <jh@poetfarmer.com>
2020-06-13 12:12:41 +0800

    add api for goodreads books

--- lib/chatbot_web/controllers/chatbot_controller.ex
+++ lib/chatbot_web/controllers/chatbot_controller.ex
@@ -20,11 +20,8 @@ defmodule ChatbotWeb.ChatbotController do
 
   def listBooks(conn, _params) do
     HTTPoison.start()
-    x = Goodreads.get!("/search/index.xml?q=mockingbird")
-    # {doc, []} = x.body |> :binary.bin_to_list() |> :xmerl_scan.string()
-    # :xmerl_xpath.string('/GoodreadsResponse/Response', term)
-    result = x.body |> xpath(~x"/GoodreadsResponse/search/results/work/best_book/title/text()"l)
-    IO.inspect(result)
+    books = Goodreads.Api.top_five_books("xzxczxczxczxcxz")
+    IO.inspect(books)
     render(conn, "hello.json")
   end
 
--- lib/chatbot_web/services/goodreads_client.ex
+++ lib/chatbot_web/services/goodreads_client.ex
@@ -9,4 +9,27 @@ defmodule Goodreads do
     param = options[:params] || %{}
     Keyword.merge(options, params: Map.merge(param, %{key: "IREJCaJWttcwlrJSqeco4g"}))
   end
+
+  defmodule Api do
+    import SweetXml
+    def top_five_books(query) do
+      get_books(query)
+      |> Enum.take(5)
+    end
+
+    def get_books(query) do
+      IO.puts "get_books"
+      encoded = URI.encode(query)
+      Goodreads.get!("/search/index.xml?q=#{encoded}")
+      |> (fn(x) -> x.body end).()
+      |>  xpath(
+          ~x"//search/results/work/best_book"l,
+          book_id: ~x"./id/text()",
+          book_title: ~x"./title/text()",
+          author_name: ~x"./author/name/text()",
+          book_image_url: ~x"./image_url/text()"
+        )
+
+    end
+  end
 end
